b3a85acff206a03be4be9bedd0daae545ca03a2f
Mudasir577 <amudasir5656@gmail.com>
2025-06-17 12:09:44 +0530

    update programme

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('This is my first project')
+//Reverse Loop 3210
+let counter;
+
+for ( counter=3; counter>=0; counter--){
+    console.log(counter)
+}
+    // looping through arrays
+
+    const vacationSpot = ['bali', 'paris', 'tulum'];
+
+    for (let i =0;i<vacationSpot.length; i++){
+        //string interpolatioin using lateral template
+        console.log(`i would love to visit ${vacationSpot[i]}` )
+    }
+// Nested loops
+
+//loop running inside another loop we call that a nested loop.
+
+const boobsFollowers = ['jannu', 'mannu', 'tannu', 'sannu'];
+const tinasFollowers = ['meena', 'mannu', 'tannu'];
+
+const mutualFollowers = [];
+
+for (let i = 0; i < boobsFollowers.length; i++) {
+    for (let j = 0; j < tinasFollowers.length; j++) {
+        if (boobsFollowers[i]===tinasFollowers[j]){
+            mutualFollowers.push(boobsFollowers[i]);
+        }
+        
+    }
+    
+}
+console.log(mutualFollowers);
