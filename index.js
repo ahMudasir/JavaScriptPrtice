ee3a2e1fd9b8935dcd5ff4d02e0477ff9119ac64
Mudasir577 <amudasir5656@gmail.com>
2025-06-17 12:46:01 +0530

    update notes

--- index.js
+++ index.js
@@ -31,3 +31,46 @@ for (let i = 0; i < boobsFollowers.length; i++) {
     
 }
 console.log(mutualFollowers);
+
+// Higher order functions
+
+//Functions as Data
+
+/*
+Javascript function behave like any other data type in the language; we can assign functions to var and we can reassign them to new variable
+example;
+*/
+
+const checkThatTwoPlusTwoEqualIsFourAMillionTimes = ()=>{
+    for (let i = 0; i <= 1000000; i++) {
+        if( ( 2 + 2 ) !=4 ){
+            console.log("Something has gone very wrong : ( ")
+        }
+        
+    }
+}
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualIsFourAMillionTimes;
+isTwoPlusTwo();
+
+console.log(isTwoPlusTwo.name)
+
+//Functions as Parameter
+
+//Call back functions
+
+const addTwo = num=>{
+ return num + 2;
+}
+
+const checkConsistentOutput = (func, val)=>{
+    const checkA = val +2;
+    const checkB = func(val);
+
+    //using ternery operator if chkA is equal to chkB . if true return the result of call back func if false use string "inconsist result"
+    return checkA===checkB ? func(val) : "inconsistent results";
+}
+//finally use console with two arguments addtwo and a number
+console.log(checkConsistentOutput(addTwo,10));
+
+// introduction to iterators 
+// .forEach()  .map() and filter()
